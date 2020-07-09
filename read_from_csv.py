e7a82ee4b7dc79568e67ace2898643e4ab7597a4
go-pedro <pgoncalves.pt@gmail.com>
2020-07-09 13:57:29 +0200

    created reader for csv

---
+++ read_from_csv.py
@@ -0,0 +1,10 @@
+import csv
+
+with open ('names.csv','r',) as csv_file:
+	csv_reader = csv.reader(csv_file)
+
+	# print(csv_reader)
+	next (csv_reader)
+	for line in csv_reader:
+		print(line[2])
+

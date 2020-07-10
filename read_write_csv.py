eaab8fd3667e46b32d9e9446c0a890338c4b9932
go-pedro <pgoncalves.pt@gmail.com>
2020-07-10 07:29:29 +0200

    change file name + DictReader/Writer

--- read_write_csv.py
+++ read_write_csv.py
@@ -2,14 +2,17 @@ import csv
 
 #open a csv file
 with open ('names.csv','r', newline='') as csv_file:
-	csv_reader = csv.reader(csv_file)
+	csv_reader = csv.DictReader(csv_file)
 
 	# for line in csv_reader:
-	# 	print (line)
+	# 	print (line['email'])
 
 	# create csv to write
 	with open ('new_names.csv','w',  newline='') as new_csv_file:
-		csv_writer=csv.writer(new_csv_file)
+		fieldnames=['first_name', 'last_name', 'email']
+		csv_writer=csv.DictWriter(new_csv_file, fieldnames=fieldnames, delimiter='\t')
+
+		csv_writer.writeheader()
 
 		for line in csv_reader:
 			csv_writer.writerow(line)
@@ -18,4 +21,3 @@ with open ('names.csv','r', newline='') as csv_file:
 	# next (csv_reader)
 	# for line in csv_reader:
 	# 	print(line[2])
-
